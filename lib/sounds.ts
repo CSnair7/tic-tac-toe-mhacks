16b78ce3c4916a80cb70e0efce3fa1548b4f3fdd
Chinmay Nair <chinmaynair008@gmail.com>
2026-09-13 14:01:48 -0400

    Added sound effects

--- lib/sounds.ts
+++ lib/sounds.ts
@@ -1,1 +1,6 @@
-// TODO: Impl playSound util
+export function playSound(name: "move" | "win" | "draw") {
+  const audio = new Audio(`/sounds/${name}.mp3`);
+  void audio.play().catch(() => {
+    // Browsers may block playback until the user has interacted with the page.
+  });
+}
